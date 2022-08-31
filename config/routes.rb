010a557fc5125fb841507bdac33e95c000e4d8dc
Tomer Harari <tharari21@gmail.com>
2022-08-30 20:09:04 -0400

    commit tree 532b196b4c2f62c836b9206426593e067a0fd524

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/plants', to: 'plants#index'
+  post '/plants', to: 'plants#create'
+  get '/plants/:id', to: 'plants#show'
+
 end
